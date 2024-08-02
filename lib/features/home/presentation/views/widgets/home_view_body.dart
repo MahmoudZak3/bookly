660a3755d4f8923e3b5e2e853ae9bc12932a979c
MahmoudZak3 <moddzak99@yahoo.com>
2024-08-02 18:43:33 +0300

    best seller item part 3

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_item_list_view.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -20,56 +22,6 @@ class HomeViewBody extends StatelessWidget {
           const Text("Best Seller", style: Styles.textStyle18),
           const SizedBox(height: 20),
           const BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            const SizedBox(height: 20),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.5,
-              child: const Text(
-                "Harry Potter and the Goblet of Fire",
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: Styles.textStyle20,
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              "Author Name",
-              style: Styles.textStyle14,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "\$20",
-              style: Styles.textStyle20,
-            )
-          ])
         ],
       ),
     );
